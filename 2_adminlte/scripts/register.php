0662c2bb5c6ecc93a076ca1b152344626169132d
m1chumeister <m1chumeister@wp.pl>
2022-11-30 07:52:25 +0100

    update

---
+++ 2_adminlte/scripts/register.php
@@ -0,0 +1,39 @@
+<?php
+    session_start();
+
+    $error = 0;
+    foreach ($_POST as $key => $value){
+        if (empty($value)){
+            $error = 1;
+        }
+    }
+
+    if (!isset($_POST['agreeTerms'])){
+        $error = 1;
+    }
+
+    if ($error == 1){
+        echo "<script>history.back()</script>";
+        exit();
+    }
+
+    require_once 'connect.php';
+    // $sql = "INSERT INTO `users` (`id`, `city_id`, `name`, `surname`, `birthday`, `created_at`, `email`, `pass`) VALUES (NULL, \'1\', \'asd\', \'asd\', \'2022-11-03\', current_timestamp(), \'asd@wp.pl\', \'pswd\');";
+    // $mysqli->query($sql);
+
+    
+    //https://www.php.net/manual/en/mysqli.quickstart.prepared-statements.php
+    $stmt = $mysqli->prepare("INSERT INTO users(city_id, name, surname, email, pass, birthday) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("isssss", $_POST['city_id'], $_POST['name'], $_POST['surname'], $_POST['email1'], $_POST['pass1'], $_POST['birthday']); // isssss -> inteager, string, string, string, string, string, string
+    
+    if ($stmt->execute()){
+        echo "ok";
+        $_SESSION['success'] = "Prawidłowo dodano użytkownika $_POST[email1]";
+    }else{
+        echo "nie ok";
+        $_SESSION['error'] = "Nie udało się dodać użytkownika";
+        exit();
+    }
+
+    header('location: ../');
+?>
---
+++ 2_adminlte/view/register.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>AdminLTE 3 | Registration Page (v2)</title>
+
+  <?php
+    require_once '../style/style.css';
+  ?>
+</head>
+<body class="hold-transition register-page">
+<div class="register-box">
+  <div class="card card-outline card-primary">
+    <div class="card-header text-center">
+      <a href="../" class="h1"><b>Admin</b>LTE</a>
+    </div>
+    <div class="card-body">
+      <p class="login-box-msg">Rejestracja użytkownika</p>
+
+      <form action="../scripts/register.php" method="post">
+
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Podaj imię" name="name">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Podaj naziwsko" name="surname">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-user"></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Podaj email" name="email1">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Powtórz email" name="email2">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Podaj hasło" name="pass1">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Powtórz hasło" name="pass2">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="input-group mb-3">
+          <input type="text" class="form-control" placeholder="Miasto" name="city_id">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-city"></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="input-group mb-3">
+          <input type="date" class="form-control" placeholder="Podaj date" name="birthday">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-calendar"></span>
+            </div>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="agreeTerms" name="agreeTerms" value="terms">
+              <label for="agreeTerms">
+               Zatwierdzam <a href="#">regulamin</a>
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Register</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <div class="social-auth-links text-center">
+        <a href="#" class="btn btn-block btn-primary">
+          <i class="fab fa-facebook mr-2"></i>
+          Sign up using Facebook
+        </a>
+        <a href="#" class="btn btn-block btn-danger">
+          <i class="fab fa-google-plus mr-2"></i>
+          Sign up using Google+
+        </a>
+      </div>
+
+      <a href="../" class="text-center">I already have a membership</a>
+    </div>
+    <!-- /.form-box -->
+  </div><!-- /.card -->
+</div>
+<!-- /.register-box -->
+
+<!-- jQuery -->
+<script src="../../plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="../../dist/js/adminlte.min.js"></script>
+</body>
+</html>
